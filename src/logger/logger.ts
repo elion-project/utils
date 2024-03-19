7dc85aa7da3e1d9d85fa9447404a4563a6b55072
Yehor Brodskiy <eqwer24@outlook.com>
2024-03-20 00:37:14 +0200

    fix: Add documentation to logger methods
    Updated `logger.ts` file to include comments on each of the logger methods.

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -76,38 +76,80 @@ export class Logger {
         }
     }
 
+    /**
+     * Logs error messages.
+     * Use this logger level to log errors that occur during your program's execution.
+     * @param message The error message(s) to log
+     */
     public error(...message: any[]) {
         this.execute("error", ...message);
     }
 
+    /**
+     * Logs warning messages.
+     * Use this logger level to log non-error messages that could potentially lead to application errors.
+     * @param message The warning message(s) to log
+     */
     public warn(...message: any[]) {
         this.execute("warn", ...message);
     }
 
+    /**
+     * Logs basic information messages.
+     * Use this logger level for informational messages to track your application's normal behavior.
+     * @param message The information message(s) to log
+     */
     public info(...message: any[]) {
         this.execute("info", ...message);
     }
 
+    /**
+     * Also logs informational messages. The same as the info logger level.
+     * @param message The message(s) to log
+     */
     public log(...message: any[]) {
         this.execute("info", ...message);
     }
 
+    /**
+     * Logs HTTP requests.
+     * Use this logger level to log the details of HTTP requests and responses.
+     * @param message The HTTP request/response message(s) to log
+     */
     public http(...message: any[]) {
         this.execute("http", ...message);
     }
 
+    /**
+     * Provides detailed logs. Use this logger level for detailed debug information beyond what you'd log for debugging.
+     * @param message The message(s) to log in verbose mode
+     */
     public verbose(...message: any[]) {
         this.execute("verbose", ...message);
     }
 
+    /**
+     * Logs debug-level messages. Use this logger level to log information helpful in debugging.
+     * @param message The debug message(s) to log
+     */
     public debug(...message: any[]) {
         this.execute("debug", ...message);
     }
 
+    /**
+     * Provides the most detailed logs.
+     * This is the highest level of logging and includes all levels of messages.
+     * @param message The message(s) to log in silly mode
+     */
     public silly(...message: any[]) {
         this.execute("silly", ...message);
     }
 
+    /**
+     * Also provides the most detailed logs.
+     * The same as the silly logger level.
+     * @param message The message(s) to log in trace mode
+     */
     public trace(...message: any[]) {
         this.execute("silly", ...message);
     }
